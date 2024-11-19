c2259ef5d46d6ab9153ea11f3e7edb7b53e0005d
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-11-19 19:48:19 +0700

    update

--- Cpp/08_arrays.cpp
+++ Cpp/08_arrays.cpp
@@ -11,8 +11,9 @@ int main()
     // Access elements
     for (int i = 0; i < 4; i++)
     {
-        cout << dob[i] << endl;
+        cout << dob[i] << "\t";
     }
+    cout << "\n";
 
     // Change array element
     dob[1] = 2024 - dob[1];
@@ -23,12 +24,25 @@ int main()
     // Array size is fixed, you cannot add or remove an element
     // For this purpose, C++ provides vectors, which are resizable arrays (from <vector> library).
     vector<string> cars = {"Volvo", "BMW", "Ford"};
-    cars.push_back("Tesla");  // Adding another element to the vector
+    cars.push_back("Tesla"); // Adding another element to the vector
+    for (int i = 0; i < 4; i++)
+    {
+        cout << cars[i] << "\t";
+    }
+    cout << "\n";
 
     // Get array size: sizeof() -> return bytes count
     int myNumbers[5] = {10, 20, 30, 40, 50};
     int getArrayLength = sizeof(myNumbers) / sizeof(int);
     cout << getArrayLength << endl;
-    
+
+    // Multi dimensional arrays
+    string letters[2][4] = {
+        { "A", "B", "C", "D" },
+        { "E", "F", "G", "H" }
+    };
+
+    cout << letters[0][0] << endl;
+
     return 0;
 }
